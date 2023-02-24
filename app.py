46a639448cb769e5c7efdb58bfdb32cf7ca0f198
cse-vaibhav <jainvaibhav671@gmail.com>
2023-02-24 08:39:29 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.write("""
+
+# Hello World
+
+I am Vaibhav Jain, an aspiring Data Scientist and ML Engineer.
+Github: [https://github.com/cse-vaibhav](Follow me on Github)
+Linkedin: [https://www.linkedin.com/in/vaibhav-jain-3b7371223/](Connect with me on Linkedin)
+DevFolio: [https://devfolio.co/@vaibhavj](My DevFolio Profile)
+""")
