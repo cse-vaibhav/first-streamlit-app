a7077453838f1e975895ddf579a961037ff497d8
cse-vaibhav <jainvaibhav671@gmail.com>
2023-02-24 08:54:43 +0530

    Trying to fix links

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
-st.write("""
+st.markdown("""
 
 # Hello World
 
 I am Vaibhav Jain, an aspiring Data Scientist and ML Engineer.\n
-Github: (Follow me on Github)[https://github.com/cse-vaibhav]\n
-Linkedin: (Connect with me on Linkedin)[https://www.linkedin.com/in/vaibhav-jain-3b7371223/]\n
-DevFolio: ("My DevFolio Profile)[https://devfolio.co/@vaibhavj]
+Github: [Follow me on Github](https://github.com/cse-vaibhav)\n
+Linkedin: [Connect with me on Linkedin](https://www.linkedin.com/in/vaibhav-jain-3b7371223/)\n
+DevFolio: [My DevFolio Profile](https://devfolio.co/@vaibhavj)
 """)
