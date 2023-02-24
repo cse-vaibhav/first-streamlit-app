a7a52507f3a32bf5ba8a6944667c38311cac88d4
cse-vaibhav <jainvaibhav671@gmail.com>
2023-02-24 08:44:48 +0530

    Updated requirements

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ st.write("""
 
 # Hello World
 
-I am Vaibhav Jain, an aspiring Data Scientist and ML Engineer.
-Github: [https://github.com/cse-vaibhav](Follow me on Github)
-Linkedin: [https://www.linkedin.com/in/vaibhav-jain-3b7371223/](Connect with me on Linkedin)
-DevFolio: [https://devfolio.co/@vaibhavj](My DevFolio Profile)
+I am Vaibhav Jain, an aspiring Data Scientist and ML Engineer.\n
+Github: [https://github.com/cse-vaibhav]("Follow me on Github")\n
+Linkedin: [https://www.linkedin.com/in/vaibhav-jain-3b7371223/]("Connect with me on Linkedin")\n
+DevFolio: [https://devfolio.co/@vaibhavj]("My DevFolio Profile")
 """)
