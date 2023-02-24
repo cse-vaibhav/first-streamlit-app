824441067d07f80baebcb4a45f1f3277e914349c
cse-vaibhav <jainvaibhav671@gmail.com>
2023-02-24 08:51:31 +0530

    Trying to fix links

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.write("""
 # Hello World
 
 I am Vaibhav Jain, an aspiring Data Scientist and ML Engineer.\n
-Github: [https://github.com/cse-vaibhav]("Follow me on Github")\n
-Linkedin: [https://www.linkedin.com/in/vaibhav-jain-3b7371223/]("Connect with me on Linkedin")\n
-DevFolio: [https://devfolio.co/@vaibhavj]("My DevFolio Profile")
+Github: (Follow me on Github)[https://github.com/cse-vaibhav]\n
+Linkedin: (Connect with me on Linkedin)[https://www.linkedin.com/in/vaibhav-jain-3b7371223/]\n
+DevFolio: ("My DevFolio Profile)[https://devfolio.co/@vaibhavj]
 """)
